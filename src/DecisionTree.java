f8d1614e16bdb21994bbadf3df8ebbc20914c201
SawyerAnderson <sawyer.lemon@gmail.com>
2013-09-30 18:05:00 -0500

    First commit

---
+++ src/DecisionTree.java
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sawyer
+ * Date: 9/30/13
+ * Time: 5:59 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DecisionTree {
+
+    //blahblahblah
+}
