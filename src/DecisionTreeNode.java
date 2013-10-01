969043d0b5f801d1921bbb18ee94090a60d7c905
SawyerAnderson <sawyer.lemon@gmail.com>
2013-09-30 23:37:06 -0500

    Finished split on entropy. Untested.

--- src/DecisionTreeNode.java
+++ src/DecisionTreeNode.java
@@ -6,14 +6,10 @@ import ml.Matrix;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 
 public class DecisionTreeNode {
-
-    public enum SplitType {
-        CONTINUOUS,
-        CATEGORICAL
-    }
 
     private Matrix features; //the features matrix for the DecisionTree this node belongs to
     private Matrix labels; //the labels matrix for the DecisionTree this node belongs to
@@ -21,10 +17,7 @@ public class DecisionTreeNode {
     private List<Integer> rows; //contains column indices to the matrices that this.features and this.labels reference
     private DecisionTreeNode leftChild;
     private DecisionTreeNode rightChild;
-    private SplitType splitType; //continuous or categorical split
-    private int splitColumn; //the index of the column we're splitting on
-    private double continuousSplitValue; //always splits on left child less than or equal to this attribute value, right child greater than
-    private String categoricalSplitValue; //left child contains this attribute value in splitColumn, right child does not
+    private SplitInformation splitInfo;
 
     public DecisionTreeNode(Matrix features, Matrix labels, List<Integer> rows){
         this.features = features;
@@ -44,26 +37,73 @@ public class DecisionTreeNode {
 
 
     public void splitOnEntropy(int k){
-        //for each column in features
-        for (int colIndex = 0; colIndex < features.getNumCols(); colIndex++){
-            //If this column is categorical, split on every possible attribute value for this column and compare entropies
-            MinMaxPriorityQueue<AttributeValueInformation> infoPriorityQueue = MinMaxPriorityQueue.create();
-            if(features.getColumnType(colIndex)== ColumnAttributes.ColumnType.CATEGORICAL){
-                ColumnAttributes columnAttributes = features.getColumnAttributes(colIndex); //get the possible attribute values for this column
-                for (int attributeValue = 0; attributeValue < columnAttributes.size(); attributeValue++){ //loop over attribute values
-                    List<List<Integer>> splitSectionIndices = splitCategoricalOnAttributeValue(colIndex, attributeValue); //split on this attribute value
-                    Double information = CalculateCategoricalInformation(splitSectionIndices); //calculate the information resulting from splitting on this attribute value
+        if (rows.size()>k && isHeterogenous()){ //if n>k and labels are heterogenous, split.
+            MinMaxPriorityQueue<SplitInformation> infoPriorityQueue = MinMaxPriorityQueue.create(); //priority queue to store attribute-value-entropy data
 
-                    infoPriorityQueue.add(new AttributeValueInformation(ColumnAttributes.ColumnType.CATEGORICAL, colIndex, (double)attributeValue, information)); //store the column-value-info in a min-heap keyed on info
+            //for each column in features
+            for (int colIndex = 0; colIndex < features.getNumCols(); colIndex++){
+                //If this column is categorical, split on every possible attribute value for this column and compare entropies
+                if(features.getColumnType(colIndex)== ColumnAttributes.ColumnType.CATEGORICAL){
+                    ColumnAttributes columnAttributes = features.getColumnAttributes(colIndex); //get the possible attribute values for this column
+                    for (int attributeValue = 0; attributeValue < columnAttributes.size(); attributeValue++){ //loop over attribute values
+                        List<List<Integer>> splitSectionIndices = splitCategorical(colIndex, attributeValue); //split on this attribute value
+                        Double information = CalculateInformation(splitSectionIndices); //calculate the information resulting from splitting on this attribute value
+
+                        infoPriorityQueue.add(new SplitInformation(ColumnAttributes.ColumnType.CATEGORICAL, colIndex, (double)attributeValue, information)); //store the column-value-info in a min-heap keyed on info
+                    }
+                }
+                else{
+                    List<Double> columnSplitValues = getRandomSample(colIndex);
+                    for (Double splitValue : columnSplitValues){ //loop over split values
+                        List<List<Integer>> splitSectionIndices = splitContinuous(colIndex, splitValue); //split on splitValue
+                        Double information = CalculateInformation(splitSectionIndices); //calculate the information resulting from splitting on splitValue
+
+                        infoPriorityQueue.add(new SplitInformation(ColumnAttributes.ColumnType.CONTINUOUS, colIndex, splitValue, information));
+                    }
                 }
             }
-            else{
-                //TODO: If this column is continuous, sample 8 random values from the column and attempt to split on those. Compare entropies.
-            }
+            SplitInformation minEntropySplit = infoPriorityQueue.peek(); //get the element with the smallest entropy in the priority queue
+            List<List<Integer>> splitSectionIndices = minEntropySplit.getColumnType() == ColumnAttributes.ColumnType.CATEGORICAL ? splitCategorical(minEntropySplit.getColumnIndex(), (minEntropySplit.getValue().intValue())) :  splitContinuous(minEntropySplit.getColumnIndex(), minEntropySplit.getValue());
+            this.leftChild = new DecisionTreeNode(features, labels, splitSectionIndices.get(0));
+            this.rightChild = new DecisionTreeNode(features, labels, splitSectionIndices.get(1));
 
-
-
+            this.leftChild.splitOnEntropy(k);
+            this.rightChild.splitOnEntropy(k);
         }
+
+    }
+
+    /**
+     * Checks to see if there is more than one label in the rows indicated by this.rows.
+     * @return
+     */
+    private boolean isHeterogenous(){
+        Double value = labels.getRow(0).get(0);
+
+        for (Integer row : rows){
+            if (!labels.getRow(row).get(0).equals(value)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns a List<Double> of a sample of 8 (or rows.size() if less than 8) values taken from the rows in this node at column colIndex
+     * @param colIndex
+     * @return
+     */
+    private List<Double> getRandomSample(int colIndex) {
+        int sampleSize = rows.size() < 8 ? rows.size() : 8; //if there aren't 8 rows in this node, just use each row's value in colIndex
+        List<Double> splitValues = new ArrayList<Double>();
+
+        Random random = new Random((long)rows.size()); //use the number of rows in this node as the seed
+        for (int i = 0; i < sampleSize; i++){
+            int rand = random.nextInt(rows.size());
+            splitValues.add(features.getRow(rows.get(rand)).get(colIndex));
+        }
+
+        return splitValues;
     }
 
     /**
@@ -72,7 +112,7 @@ public class DecisionTreeNode {
      * @param attributeValue
      * @return a List<List<Integer>> of size 2, where index 0 is the matching row indices, and index 1 is the non-matching row indices
      */
-    private List<List<Integer>> splitCategoricalOnAttributeValue(Integer columnIndex, Integer attributeValue){
+    private List<List<Integer>> splitCategorical(Integer columnIndex, Integer attributeValue){
         List<List<Integer>> sections = new ArrayList<List<Integer>>();
         List<Integer> matchingRowIndices = new ArrayList<Integer>(); //the list of rows where row.get(columnIndex) == attributeValue
         List<Integer> nonmatchingRowIndices = new ArrayList<Integer>(); //the list of rows where row.get(columnIndex) != attributeValue
@@ -94,8 +134,35 @@ public class DecisionTreeNode {
         return sections;
     }
 
-    private Double CalculateCategoricalEntropy(List<Integer> rows, ColumnAttributes labelColumnAttributes){
-        Counter<Double> counter = new Counter<Double>();
+    /**
+     * Returns a List of two List<Integer>s, representing the rows in this.rows split on the column-splitValue pair
+     * @param colIndex
+     * @param splitValue
+     * @return a List<List<Integer>> of size 2, where index 0 is the <= row indices, and index 1 is the > indices
+     */
+    private List<List<Integer>> splitContinuous(int colIndex, Double splitValue) {
+        List<List<Integer>> sections = new ArrayList<List<Integer>>();
+        List<Integer> matchingRowIndices = new ArrayList<Integer>(); //the list of rows where row.get(columnIndex) <= attributeValue
+        List<Integer> nonmatchingRowIndices = new ArrayList<Integer>(); //the list of rows where row.get(columnIndex) > attributeValue
+
+        //populate the List<List<Integer>>
+        sections.add(0, matchingRowIndices);
+        sections.add(1, nonmatchingRowIndices);
+
+        //Populate the two List<Integer>s
+        for (Integer rowIndex : this.rows){
+            if (this.features.getRow(rowIndex).get(colIndex)<=splitValue){
+                matchingRowIndices.add(rowIndex);
+            }
+            else{
+                nonmatchingRowIndices.add(rowIndex);
+            }
+        }
+        return sections;
+    }
+
+    private Double CalculateEntropy(List<Integer> rows, ColumnAttributes labelColumnAttributes){
+        Counter<Double> counter = new Counter<Double>(); //counts the number of times each possible label attribute-value occurs
         int numAttrValues = labelColumnAttributes.size();
 
         //zero the counts for each possible label attribute value
@@ -118,12 +185,12 @@ public class DecisionTreeNode {
         return entropy;
     }
 
-    private Double CalculateCategoricalInformation(List<List<Integer>> sections){
+    private Double CalculateInformation(List<List<Integer>> sections){
         Double matchingRatio = ((double)sections.get(0).size())/rows.size();
         Double nonmatchingRatio = ((double)sections.get(1).size())/rows.size();
 
-        Double matchingEntropy = CalculateCategoricalEntropy(sections.get(0), labels.getColumnAttributes(0));
-        Double nonmatchingEntropy = CalculateCategoricalEntropy(sections.get(1), labels.getColumnAttributes(0));
+        Double matchingEntropy = CalculateEntropy(sections.get(0), labels.getColumnAttributes(0));
+        Double nonmatchingEntropy = CalculateEntropy(sections.get(1), labels.getColumnAttributes(0));
 
         return (matchingRatio*matchingEntropy)+(nonmatchingRatio*nonmatchingEntropy);
     }
@@ -150,22 +217,6 @@ public class DecisionTreeNode {
         this.leftChild = leftChild;
     }
 
-    public SplitType getSplitType() {
-        return splitType;
-    }
-
-    public void setSplitType(SplitType splitType) {
-        this.splitType = splitType;
-    }
-
-    public int getSplitColumn() {
-        return splitColumn;
-    }
-
-    public void setSplitColumn(int splitColumn) {
-        this.splitColumn = splitColumn;
-    }
-
     public DecisionTreeNode getRightChild() {
         return rightChild;
     }
@@ -174,19 +225,4 @@ public class DecisionTreeNode {
         this.rightChild = rightChild;
     }
 
-    public double getContinuousSplitValue() {
-        return continuousSplitValue;
-    }
-
-    public void setContinuousSplitValue(double continuousSplitValue) {
-        this.continuousSplitValue = continuousSplitValue;
-    }
-
-    public String getCategoricalSplitValue() {
-        return categoricalSplitValue;
-    }
-
-    public void setCategoricalSplitValue(String categoricalSplitValue) {
-        this.categoricalSplitValue = categoricalSplitValue;
-    }
 }
--- src/SplitInformation.java
+++ src/SplitInformation.java
@@ -1,12 +1,12 @@
 import ml.ColumnAttributes;
 
-public class AttributeValueInformation implements Comparable<AttributeValueInformation>{
+public class SplitInformation implements Comparable<SplitInformation>{
     private ColumnAttributes.ColumnType columnType;
     private Integer columnIndex;
     private Double value;
     private Double information;
 
-    public AttributeValueInformation(ColumnAttributes.ColumnType columnType, Integer columnIndex, Double value, Double information){
+    public SplitInformation(ColumnAttributes.ColumnType columnType, Integer columnIndex, Double value, Double information){
         this.columnType = columnType;
         this.columnIndex = columnIndex;
         this.value = value;
@@ -47,7 +47,7 @@ public class AttributeValueInformation implements Comparable<AttributeValueInfor
     }
 
     @Override
-    public int compareTo(AttributeValueInformation o) {
+    public int compareTo(SplitInformation o) {
         return Double.compare(this.information, o.getInformation());
     }
 }
